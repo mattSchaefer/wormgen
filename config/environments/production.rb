a9387e3fbde1ca50a05347daa7670fc1d064b52b
Matt Schaefer <matthew.schaefer12@gmail.com>
2022-02-05 09:21:49 -0500

    no master key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
 
 
 
-  config.require_master_key = true 
+  #config.require_master_key = true 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
